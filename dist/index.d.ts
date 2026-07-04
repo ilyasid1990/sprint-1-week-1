ed0b07d4f9990ec9e93d60a49757bf89017d89af
ilyasid1990 <79026851552@yandex.ru>
2026-07-04 23:54:57 +0300

    Change commit 10

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import { type Express } from "express";
 export declare const app: Express;
+export default app;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -9,4 +9,5 @@ const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
+export default app;
 //# sourceMappingURL=index.js.map
